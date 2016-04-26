412ce6218fe4e4514b5f0fc77d3a4a5d7e23cfd7
belilung <bobnyuk.andrei@yandex.ru>
2016-04-27 00:37:37 +0300

    first

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href='https://api.mapbox.com/mapbox.js/v2.3.0/mapbox.css' rel='stylesheet' />
+    <link href='css/style.css' rel='stylesheet' />
+</head>
+<body>
+
+<div id='map'></div>
+=(
+</body>
+
+<script src='https://api.mapbox.com/mapbox.js/v2.3.0/mapbox.js'></script>
+<script src='js/map.js'></script>
+</html>
